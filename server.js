d6d0d9f036db6e34675c8a0cef3dc971b42c682e
Kumuda Priya <cs14btech11029@iith.ac.in>
2016-06-20 17:27:28 +0530

    Added login route and authenticate class method

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force : true}).then(function() {
 
 	app.listen(PORT, function() {
 		console.log("server is working");
